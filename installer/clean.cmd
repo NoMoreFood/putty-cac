d2b487497e5efee8a43010eeb4bdd47fec67ac3c
Bryan Berns <berns@uwalumni.com>
2017-03-16 08:23:03 -0400

    Added File To Remove Visual Studio Artifacts

---
+++ installer/clean.cmd
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+FOR %%X IN (Win32 x64 Debug Release .vs) DO (
+  FORFILES /S /P "%~dp0..\windows" /M "%%X" /C "CMD /C IF @isdir==TRUE RD /S /Q @path"
+)
+FOR %%X IN (Win32 x64 Debug Release) DO (
+  FORFILES /S /P "%~dp0..\executables" /M "*.pdb" /C "CMD /C DEL /Q @path"
+)
+
+PAUSE
