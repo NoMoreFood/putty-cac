80b360ca9aab6634b8d611ab00a2369dde7f3170
Bryan Berns <berns@uwalumni.com>
2017-07-08 18:22:43 -0400

    Merged Changes From PuTTY 0.70
    - Merged upstream changes from PuTTY 0.70
    - Added proper Visual Studio version detection for the About box
    - Built binaries for PuTTY CAC 0.70

--- packager/build.cmd
+++ packager/build.cmd
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 :: version information
-SET VER=0.69u1
-SET VERN=0.69.0.1
+SET VER=0.70
+SET VERN=0.70.0.0
 
 :: cert info to use for signing
 SET CERT=9CC90E20ABF21CDEF09EE4C467A79FD454140C5A
