46351884a08026b80197703ef5781120dd3d955f
Bryan Berns <berns@uwalumni.com>
2018-01-17 09:07:42 -0500

    Updated Binaries For 0.70u3
    - Recompiled binaries based on latest code changes
    - Removed unnecessary header reference in project to unix.h
    - Changed project properties to omit frame pointers (compiler optimization)
    - Adjusted build batch file latest Windows SDK version

--- packager/build.cmd
+++ packager/build.cmd
@@ -22,7 +22,7 @@ IF DEFINED ProgramFiles(x86) SET PX86=%ProgramFiles(x86)%
 
 :: setup paths
 SET PATH=%WINDIR%\system32;%WINDIR%\system32\WindowsPowerShell\v1.0
-SET PATH=%PATH%;%PX86%\Windows Kits\10\bin\x64
+SET PATH=%PATH%;%PX86%\Windows Kits\10\bin\10.0.16299.0\x64
 SET PATH=%PATH%;%PX86%\Windows Kits\8.1\bin\x64
 SET PATH=%PATH%;%PX86%\WiX Toolset v3.11\bin
 
