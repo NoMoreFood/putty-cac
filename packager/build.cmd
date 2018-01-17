914b19bb83a8d79c6c64b867ecf2e725f68a972a
Bryan Berns <berns@uwalumni.com>
2018-01-17 00:31:12 -0500

    Pageant Enhancements
    - Added workaround for loading a certificate from command line when Pageant is already running
    - Added button to Pageant key list to copy the SSH keystring to the clipboard
    - Added '-autoload' command line parameter for Pageant
    - Disabled C++ exception handling to reduce executable size
    - Updated version information for 0.70 Update 3

--- packager/build.cmd
+++ packager/build.cmd
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 :: version information
-SET VER=0.70u2
-SET VERN=0.70.0.2
+SET VER=0.70u3
+SET VERN=0.70.0.3
 
 :: cert info to use for signing
 SET CERT=9CC90E20ABF21CDEF09EE4C467A79FD454140C5A
