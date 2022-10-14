6feb463cb7b961c99b44cc510fd9d105de46081a
Bryan Berns <berns@uwalumni.com>
2022-10-14 18:04:58 -0400

    Address Pageant Command Line Issue

--- packager/chocolatey/tools/chocolateyinstall.ps1
+++ packager/chocolatey/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/NoMoreFood/putty-cac/raw/0.77u1/binaries/puttycac-0.77u1-installer.msi'
-$url64      = 'https://github.com/NoMoreFood/putty-cac/raw/0.77u1/binaries/puttycac-64bit-0.77u1-installer.msi'
+$url        = 'https://github.com/NoMoreFood/putty-cac/raw/0.77u2/binaries/puttycac-0.77u2-installer.msi'
+$url64      = 'https://github.com/NoMoreFood/putty-cac/raw/0.77u2/binaries/puttycac-64bit-0.77u2-installer.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -12,11 +12,11 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
 
-  softwareName  = 'puTTY CAC*'
+  softwareName  = 'PuTTY CAC*'
 
-  checksum      = '907F201234DE8906276B0EEB9A2CD8363FB64047F0607046D6C45EF12AED8F7E'
+  checksum      = '44D0C15BC5E407C3E0160B79246F7D1722FB0994FD7FA65EE8CB1F77CE28E65E'
   checksumType  = 'sha256'
-  checksum64    = '48076176FA7D09A0A7EB51D541B736A632FFE6A709A7DF388B95398F0D8284ED'
+  checksum64    = '9ADABD6C7C354EA01866B3083B6697782D35CE959156988BCC80E42A41A3D72C'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
