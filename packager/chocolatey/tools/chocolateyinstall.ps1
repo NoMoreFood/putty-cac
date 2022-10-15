7052d2998d4d689ebd5c36ddfec2abffd5d46252
Bryan Berns <berns@uwalumni.com>
2022-10-15 08:29:14 -0400

    Address Inconsistent Memory Zeroing

--- packager/chocolatey/tools/chocolateyinstall.ps1
+++ packager/chocolatey/tools/chocolateyinstall.ps1
@@ -14,9 +14,9 @@ $packageArgs = @{
 
   softwareName  = 'PuTTY CAC*'
 
-  checksum      = '44D0C15BC5E407C3E0160B79246F7D1722FB0994FD7FA65EE8CB1F77CE28E65E'
+  checksum      = '5590A2B63F5AC87FFEF6BAFBF45AD4B97EE32411B3CE2FCBFBB257E7D34C6175'
   checksumType  = 'sha256'
-  checksum64    = '9ADABD6C7C354EA01866B3083B6697782D35CE959156988BCC80E42A41A3D72C'
+  checksum64    = 'E17EEBC4943F90AC4A48DD99A9A60000FD3908F92F562979F4189D2E8767B9D5'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
