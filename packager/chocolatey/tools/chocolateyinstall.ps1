5bbc8e2dc1a7e0bdb55857760634cd6c7194b1f7
Bryan Berns <berns@uwalumni.com>
2023-02-10 14:16:18 -0500

    PuTTY 0.78 Update 1 Release
    - Disable DLL hijacking protection due to incompatible with SecurID and ActivClient incompatibilities.

--- packager/chocolatey/tools/chocolateyinstall.ps1
+++ packager/chocolatey/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/NoMoreFood/putty-cac/raw/0.78/binaries/puttycac-0.78-installer.msi'
-$url64      = 'https://github.com/NoMoreFood/putty-cac/raw/0.78/binaries/puttycac-64bit-0.78-installer.msi'
+$url        = 'https://github.com/NoMoreFood/putty-cac/raw/0.78u1/binaries/puttycac-0.78u1-installer.msi'
+$url64      = 'https://github.com/NoMoreFood/putty-cac/raw/0.78u1/binaries/puttycac-64bit-0.78u1-installer.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,9 +14,9 @@ $packageArgs = @{
 
   softwareName  = 'PuTTY CAC*'
 
-  checksum      = '5B788BBDEE9D1926F39576EB786E0CDB207B599788B70634AF87203C0F9149F4'
+  checksum      = 'C8DF457BE4AD9D3F078BE01A6A6272A641DF57B845474B899ED5B0AD5E6AC930'
   checksumType  = 'sha256'
-  checksum64    = 'E43DCD6D976CC2132F849DDB5F20EBF82BD2FDB6B509B90D70B14FF113FE8866'
+  checksum64    = 'C2E35BD6619EF090C983BE644FC606B858083B5A47CF03852A843B3ECFEBC18D'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
