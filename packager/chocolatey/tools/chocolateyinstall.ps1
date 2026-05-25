b2fe6c95f01e92c3037e6dad7376bd14e6a72519
Bryan Berns <berns@uwalumni.com>
2026-05-24 20:06:23 -0400

    Cleanup Superseded Packager Artifacts

--- packager/chocolatey/tools/chocolateyinstall.ps1
+++ packager/chocolatey/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/NoMoreFood/putty-cac/raw/0.83u2/binaries/puttycac-0.83u2-x86.msi'
-$url64      = 'https://github.com/NoMoreFood/putty-cac/raw/0.83u2/binaries/puttycac-0.83u2-x64.msi'
+$url        = 'https://github.com/NoMoreFood/putty-cac/raw/0.0/binaries/puttycac-0.0-x86.msi'
+$url64      = 'https://github.com/NoMoreFood/putty-cac/raw/0.0/binaries/puttycac-0.0-x64.msi'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,9 +14,9 @@ $packageArgs = @{
 
   softwareName  = 'PuTTY CAC*'
 
-  checksum      = '0BA5265C903BA6DE813A7BB0CED4CD6AA05A10A245045F6696181938C83DB6C9'
+  checksum      = '0000000000000000000000000000000000000000000000000000000000000000'
   checksumType  = 'sha256'
-  checksum64    = 'B49B5B4540A9633D1864560A4073A66EC28ECF227515584BCEE347FA581A6D47'
+  checksum64    = '0000000000000000000000000000000000000000000000000000000000000000'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
